c17a2b0db9196ab2db8d45c2c705242e9922f012
Ongenix <96583029+Ongenix@users.noreply.github.com>
2022-08-20 14:45:49 -0600

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,1 @@
+print(('-'*54)+'Calculator!');print('+');print('-');print('*');print('/');x = input('> ');num1 = int(input('Number 1?\n'));num2 = int(input('Number 2?\n'));print(eval(f'{num1}{x}{num2}'))
